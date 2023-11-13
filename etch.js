d42e0a91aea4f0dcfa1831971ce942bc47297e0d
Alan Maginn <p.alan.maginn@gmail.com>
2023-11-13 11:26:15 -0500

    Add columns and prompt for user input

--- etch.js
+++ etch.js
@@ -1,39 +1,27 @@
-// create a container for the grid
-document.querySelector('#grid');
+let userInput = prompt('How many rows and columns would you like on your etch-a-sketch?')
 
-// create the grid
+
 let manyBoxes = function() {
-	for (let i = 0; i < 16; i++) {
+  // Loop for rows
+    for (let i = 0; i < userInput; i++) { 
+        const row = document.createElement('div');
+        row.classList.add('row');
+        grid.appendChild(row);
+  // Loop for columns
+    for (let j = 0; j < userInput; j++) { 
+        const singleBox = document.createElement('div');
+        singleBox.classList.add('singleBox');
+        row.appendChild(singleBox);
 
-		// create the grid
-		const singleBox = document.createElement('div');
-		singleBox.classList.add('singleBox');
-		grid.appendChild(singleBox);
-
-		// color the grid
-			let colorBox = function() {
-				let randomColor = Math.floor(Math.random()*16777215).toString(16);
-				singleBox.style.backgroundColor = '#' + randomColor;
-			}
-			singleBox.addEventListener('mouseover', colorBox);
-	}
+  // color the grid
+     let colorBox = function() {
+          let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            singleBox.style.backgroundColor = '#' + randomColor;
+            }
+             singleBox.addEventListener('mouseover', colorBox);
+        }
+  }  
 }
-
 manyBoxes();
-
-// need to write a function that makes a square grid (ie 4 x 4 or 8 x 8) dependent upon user input
-// first create a div that changes when hovered over with the mouse down to a black box
-
-// Add the event listener for coloring the individual box
-
-// let colorBox = function() {
-// 	let randomColor = Math.floor(Math.random()*16777215).toString(16);
-// 	singleBox.style.backgroundColor = '#' + randomColor;
-// }
-// singleBox.addEventListener('mouseover', colorBox);
-
-// const singleBox = document.createElement('div');
-
-// gridContainer.appendChild(singleBox);
 
 
